b9d507cd6386b2f788e57d4b5ff88a662201458a
bonnichiwa <bonnie.pham@ryerson.ca>
2015-08-23 16:17:37 -0400

    added generating random number function

--- js/app.js
+++ js/app.js
@@ -1,23 +1,30 @@
 
-$(document).ready(function(){
-	
-	/*--- Display information modal box ---*/
-  	$(".what").click(function(){
-    	$(".overlay").fadeIn(1000);
-
-  	});
-
-  	/*--- Hide information modal box ---*/
-  	$("a.close").click(function(){
-  		$(".overlay").fadeOut(1000);
-  	});
-
+$(document).ready(function() {
+  gameInstruction();
+  newGame();
 });
 
+function gameInstruction() {
+    /*--- Display information modal box ---*/
+    $(".what").click(function(){
+      $(".overlay").fadeIn(1000);
 
-function newGame {
+    });
+
+    /*--- Hide information modal box ---*/
+    $("a.close").click(function(){
+      $(".overlay").fadeOut(1000);
+    });
+}
+
+function newGame() {
   $("a.new").click(function(){
     $("span#count").val('0');
     $("ul#guessList").empty();
+    generateNumber(1, 100);
   });
+}
+
+function generateNumber(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
 }
