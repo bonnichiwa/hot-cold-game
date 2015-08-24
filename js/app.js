eb683e512c39b277ca8bdb1c5130757a46a94ec3
bonnichiwa <bonnie.pham@ryerson.ca>
2015-08-24 16:21:16 -0400

    verfiy numbers by using isNumeric, tried to build functions to output feedback

--- js/app.js
+++ js/app.js
@@ -1,11 +1,14 @@
 
 $(document).ready(function() {
+  
   gameInstruction();
+  generateNumber();
   newGame();
-  giveFeedback();
-});
+  guessComparison();
+  validateGuess();
 
-function gameInstruction() {
+
+  function gameInstruction() {
     /*--- Display information modal box ---*/
     $(".what").click(function(){
       $(".overlay").fadeIn(1000);
@@ -16,33 +19,114 @@ function gameInstruction() {
     $("a.close").click(function(){
       $(".overlay").fadeOut(1000);
     });
-}
+  }
 
-function newGame() {
-  $("a.new").click(function(){
-    $("span#count").text('0');
-    $("ul#guessList").empty();
-    generateNumber(1, 100);
-  });
-}
+  /*--- Random number generator--- */
 
-function generateNumber(min, max) {
-  console.log(Math.floor(Math.random() * (max - min)) + min);
-}
+  function generateNumber() {
+    randomNumber = (Math.floor(Math.random() * 100));
+    console.log("Random number generated = " + randomNumber);
+  }
 
-function giveFeedback() { 
-  $("#guessButton").click(function() {
-    var currentGuess = $("#userGuess").val();
-    var parsedGuess = parseInt(currentGuess);
-    if(parsedGuess !== NaN) {
-      $("ul#guessList").append("<li>"+ parsedGuess + "</li>");
-      $("span#count").text($("ul#guessList > li").size());
-      $("#userGuess").val('');
-    return false;
+  /*--- New game ---*/
+
+  function newGame() {
+    $("a.new").click(function(){
+      $("span#count").text('0');
+      $("ul#guessList").empty();
+    });
+  }
+
+  /*--- validate if guess is valid number ---*/
+
+  function validateGuess() {
+    $("#guessButton").click(function() {
+      var currentGuess = $("#userGuess").val();
+      if(($.isNumeric(currentGuess)) && (currentGuess >= 0) && (currentGuess <= 100)) {
+        $("ul#guessList").append("<li>"+ currentGuess + "</li>");
+        $("span#count").text($("ul#guessList > li").size());
+        $("#userGuess").val('');
+        console.log("User guess = " + currentGuess);
+        guessComparison();
+        return false;
+      } else {
+        alert("You need to enter a valid number.");
+        $("#userGuess").val('');
+        return false;
+      }
+    });
+  }
+
+  /*--- Determine if using negative or positive comparison ---*/
+  function guessComparison() {
+    if (randomNumber - $("#userGuess").val() > 0) {
+      positiveComparison();
     } else {
-      alert("You need to enter a valid number.");
-      $("#userGuess").val('');
-    return false;
+      negativeComparison();
     }
-  });
-}
+  }
+
+
+  /*--- Set feedback ---*/
+
+  function setFeedback() {
+    $("h2#feedback").text(feedback);
+  }
+
+  /*--- Feedback changed according to user input ---*/
+
+  function positiveComparison() { 
+    var generateDiff = randomNumber - $("userGuess").val();
+    console.log(generateDiff);  
+    switch(true) {
+    case(generateDiff >= 50) :
+      setFeedback("Ice Cold");
+      break;
+    case((generateDiff >= 30) && (generateDiff <= 49)):
+      setFeedback('Cold');
+      break;
+    case((generateDiff >= 20) && (generateDiff <= 29)):
+      setFeedback('Warm');
+      break;
+    case((generateDiff >= 10) && (generateDiff <= 19)):
+      setFeedback('Hot');
+      break;
+    case((generateDiff >= 1) && (generateDiff <= 9)):
+      setFeedback('Boiling Hot!');
+      break;
+    case(generateDiff = 0):
+      alert("Congrats, you've beat the game!");
+      setFeedback('Bulls Eye!');
+      break;
+    }
+  }
+
+  function negativeComparison() {
+    var generateDiff = currentGuess - randomNumber;
+    console.log(generateDiff);  
+    switch(true) {
+    case(generateDiff >= 50) :
+      setFeedback("Ice Cold");
+      break;
+    case((generateDiff >= 30) && (generateDiff <= 49)):
+      setFeedback('Cold');
+      break;
+    case((generateDiff >= 20) && (generateDiff <= 29)):
+      setFeedback('Warm');
+      break;
+    case((generateDiff >= 10) && (generateDiff <= 19)):
+      setFeedback('Hot');
+      break;
+    case((generateDiff >= 1) && (generateDiff <= 9)):
+      setFeedback('Boiling Hot!');
+      break;
+    case(generateDiff = 0):
+      alert("Congrats, you've beat the game!");
+      setFeedback('Bulls Eye!');
+      break;
+    }
+  }
+
+  
+
+});
